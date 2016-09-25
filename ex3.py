54ce12d233af797249eca333174386cc772c0b63
Brett Goss <brettgoss@live.com>
2016-09-25 21:26:06 +0000

    exercises 1 - 4 completed

---
+++ ex3.py
@@ -0,0 +1,33 @@
+# Prints string
+print "I will now count my chickens:"
+
+# Printing a string, followed by a calculation on the same line
+print "Hens", 25.0 + 30.0 / 6.0
+print "Roosters", 100.0 - 25.0 * 3.0 % 4.0
+
+# Printing another string
+print "I will now count the eggs:"
+
+# Counting all the eggs. Way more operations than necessary
+print 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Printing a string asking if math is true
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Actually doing the math from the above string
+print 3.0 + 2.0 < 5.0 - 7.0
+
+# Breaking apart the above comparison into the two sides
+print "What is 3 + 2?", 3.0 + 2.0
+print "What is 5 - 7?", 5.0 - 7.0
+
+# Printing a string
+print "Oh, that's why it's False."
+
+# Printing another string
+print "How about some more."
+
+# Directly doing different comparisons of the above math
+print "Is it greater?", 5.0 > -2.0
+print "Is it greater or equal?", 5.0 >= -2.0
+print "Is it less or equal?", 5.0 <= -2.0
